56f4b5c437d4e20216786138db0b28c0cc213207
Nikola Tosic <tosicnikola10@gmail.com>
2022-03-20 10:50:46 +0100

    tested versions 3.6+

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup
 
-setup(name='aws-keys-sectool', version='0.1.0', author='Nikola Tosic',
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(name='aws-keys-sectool', version='0.1.1', author='Nikola Tosic',
       author_email='nikola@toshke.me',
       url='https://github.com/toshke/aws-keys-security',
       classifiers=[
           'Development Status :: 3 - Alpha',
           'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10',
           'Intended Audience :: System Administrators',
           'Intended Audience :: Information Technology',
           'License :: OSI Approved :: MIT License',
